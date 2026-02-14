6e9a078e3fc4de24a28dfcc5954dfb4be692eccc
Dragonghgh <jricke10@gmail.com>
2026-02-14 17:41:44 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,52 +1,27 @@
-const config = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: 'arcade',
-    arcade: { debug: false }
-  },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
-};
-
-const game = new Phaser.Game(config);
-let player;
-let cursors;
-
 function preload() {
-  this.load.image('player', 'assets/player.png'); // add your pixel player
-  this.load.image('tiles', 'assets/tileset.png'); // placeholder tiles
+  this.load.image('player', 'assets/player.png');
+  this.load.image('tiles', 'assets/tileset.png');
+  this.load.tilemapTiledJSON('map', 'assets/map.json'); // load map
 }
 
+let map;
+let layer;
+
 function create() {
-  // Add player sprite
+  map = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
+  const tileset = map.addTilesetImage('tiles');
+
+  layer = map.createLayer('Ground', tileset, 0, 0);
+
+  // Make walls solid
+  layer.setCollision([2]); // tile index 1 for walls (Phaser starts counting at 0)
+  
   player = this.physics.add.sprite(400, 300, 'player');
   player.setCollideWorldBounds(true);
 
-  // Keyboard input
+  this.physics.add.collider(player, layer);
+
   cursors = this.input.keyboard.addKeys({
-    up: 'W',
-    down: 'S',
-    left: 'A',
-    right: 'D',
-    interact: 'E'
+    up: 'W', down: 'S', left: 'A', right: 'D', interact: 'E'
   });
-}
-
-function update() {
-  player.setVelocity(0);
-
-  if (cursors.left.isDown) player.setVelocityX(-150);
-  if (cursors.right.isDown) player.setVelocityX(150);
-  if (cursors.up.isDown) player.setVelocityY(-150);
-  if (cursors.down.isDown) player.setVelocityY(150);
-
-  // Interact key
-  if (Phaser.Input.Keyboard.JustDown(cursors.interact)) {
-    console.log("Interacted!");
-  }
 }
